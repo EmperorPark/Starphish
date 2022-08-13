7451a8c7c6a854911c8f7ddbb8a1f5b334523e03
EmperorPark <woghkd0719@gmail.com>
2022-08-13 21:45:33 +0900

    디자인 요소 변경

--- popup.js
+++ popup.js
@@ -1,7 +1,1 @@
-var canvas = document.getElementById("drawCanvas");
-    var context = canvas.getContext("2d");
-    var text = "고슴도치";
-
-    context.font ="35pt Fira";
-    context.fillStyle = "rgb(0, 0, 0)";
-    context.fillText(text, 1, 40);
+// 안써서 내용 삭제 나중에 활용할 가능성 있으니 지금은 남겨둠
