830026ba14bc67ead072bca21f2c36b85c46326d
Han Donguk <davidde@naver.com>
2022-08-13 15:37:15 +0900

    common / index / manifest / popup
    기본 작동 js파일 생성, test용 index.js, manifest 타겟위치 설정

--- common.js
+++ common.js
@@ -1,1 +1,5 @@
 //공통함수파일
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let hello = "Hello World!"
 
 console.log(hello);
+
+console.log( getElementByXpath("//html[1]/body[1]/div[1]") );
