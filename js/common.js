c20502a866c84bdd46f15010abc38a932ec6c9b2
EmperorPark <woghkd0719@gmail.com>
2022-08-16 21:23:46 +0900

    실험중이던 기능제거

--- js/common.js
+++ js/common.js
@@ -8,10 +8,10 @@ function common() {
 
 common.prototype.delHref = function(areaStr) {
     
-    if(document.querySelector(areaStr) !== null) {
-        if(document.querySelector("#starPhishAnnounce") !== null) document.querySelector("#starPhishAnnounce").remove();
-        return;
-    }
+    // if(document.querySelector(areaStr) !== null) {
+    //     if(document.querySelector("#starPhishAnnounce") !== null) document.querySelector("#starPhishAnnounce").remove();
+    //     return;
+    // }
 
 
     document.querySelectorAll(areaStr + " a[href]").forEach(item => {
@@ -23,13 +23,13 @@ common.prototype.delHref = function(areaStr) {
         item.setAttribute("title", "해당 링크는 StarPhish에 의해 비활성화 되었습니다.\n피싱으로 인한 사기 및 해킹 방지를 위해 링크를 사용하지 않고 메일의 목적을 판단할 수 있는 경우 링크를 사용하지 않는것이 권장됩니다.\n링크를 이용하셔야 하는 경우 해당 메일과 링크의 안전함을 판단 및 확인 후 링크를 이용하시기 바랍니다.\n(해당 링크: " + item.getAttribute("href_bak") + ")");
     });
 
-    let starPhishAnnounceNode = document.createElement("h1");
-    starPhishAnnounceNode.setAttribute("id", "starPhishAnnounce");
-    starPhishAnnounceNode.textContent = "해당 메일은 starPhish의 보안설정에 의해 변경되었습니다."
+    // let starPhishAnnounceNode = document.createElement("h1");
+    // starPhishAnnounceNode.setAttribute("id", "starPhishAnnounce");
+    // starPhishAnnounceNode.textContent = "해당 메일은 starPhish의 보안설정에 의해 변경되었습니다."
     
-    if(document.querySelector("#starPhishAnnounce") === null) {
-        document.querySelector(areaStr).insertBefore(starPhishAnnounceNode, document.querySelector(areaStr).firstChild);
-    }
+    // if(document.querySelector("#starPhishAnnounce") === null) {
+    //     document.querySelector(areaStr).insertBefore(starPhishAnnounceNode, document.querySelector(areaStr).firstChild);
+    // }
 }
 
 common.prototype.setIframeList = function(area) {
@@ -76,3 +76,7 @@ common.prototype.googleAmpMailChange = function() {
 }
 
 
+common.prototype.emailContentToXmp = function(areaStr) {
+    let node = document.querySelector(areaStr);
+    node.outerHTML = "<xmp>" + node.outerHTML + "</xmp>";
+}
