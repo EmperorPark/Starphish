12087d8f3a77b28f306ef5f1c47d68a552ab36c1
EmperorPark <woghkd0719@gmail.com>
2022-08-18 21:41:53 +0900

    메뉴얼 작업 중간 커밋

--- background.js
+++ background.js
@@ -1,8 +1,18 @@
+
+let mode = 1;
 // 0: off
 // 1: remove link
-// 2: html raw
-let mode = 1;
-
+// 2: html textContent
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ mode:mode });
+});
+
+
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        chrome.runtime.openOptionsPage();
+    }
+    else if(details.reason == "update"){
+        chrome.runtime.openOptionsPage();
+    }
 });
